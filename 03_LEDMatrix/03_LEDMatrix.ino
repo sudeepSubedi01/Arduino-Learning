034b065e6363bf64a7aa1d19efaef675e8d17e36
Sudip Subedi <sudip.subedi99@gmail.com>
2024-06-10 21:17:07 +0545

    letter C complete

--- 03_LEDMatrix/03_LEDMatrix.ino
+++ 03_LEDMatrix/03_LEDMatrix.ino
@@ -4,26 +4,14 @@ const int hc=2;
 int rowPins[numRows] = {4,5,6,7,8, 9, 10, 11}; // Rows are connected to pins 8, 9, 10, 11, 12
 int colPins[numCols] = {12,13,A5,A4,A3,A2,A1,A0};    // Columns are connected to pins 3, 4, 5, 6, 7
 
-String letterH_vertical[hc] = {
-  	"10011001",
-    "10000001" 
-};
+String letterH_vertical[hc] = {"10011001","10000001"};
+String letterH_horizontal[hc] = {"11111111","00011000"};
 
-String letterH_horizontal[hc] = {
-  "11111111",
-  "00011000"
-};
+String letterE_vertical[hc] = {"00111111","00000000"};
+String letterE_horizontal[hc] = {"11111111","11011011"};
 
-String letterE_vertical[hc] = {
-  	"00111111",
-    "00000000" 
-};
-
-String letterE_horizontal[hc] = {
-  "11111111",
-  "11011011"
-};
-
+String letterC_vertical[hc] = {"00000000", "00111111"};
+String letterC_horizontal[hc] = {"11000011", "11111111"};
 void setup() {
   Serial.begin(9600);
   for (int i = 0; i < numRows; i++) {
@@ -36,15 +24,35 @@ void setup() {
 
 void loop() {
   while (true) {
-    displayLetterH();
+    //displayLetterH();
+    //delay(1000);
+    //displayLetterE();
+    //delay(1000);
+    displayLetterC();
   }
 }
 
+void displayLetterE() {
+  for (int i = 0; i < hc; i++) {
+    for (int j = 0; j < numCols; j++) {
+      if (letterE_vertical[i].charAt(j) == '0') {
+        digitalWrite(colPins[j], LOW);
+      } else {
+        digitalWrite(colPins[j], HIGH);
+      }
+      if (letterE_horizontal[i].charAt(j) == '1') {
+        digitalWrite(rowPins[j], HIGH);
+      } else {
+        digitalWrite(rowPins[j], LOW); 
+      }
+    }
+    delay(2); 
+  }
+}
 void displayLetterH() {
   for (int i = 0; i < hc; i++) {
     for (int j = 0; j < numCols; j++) {
       if (letterH_vertical[i].charAt(j) == '0') {
-        
         digitalWrite(colPins[j], LOW);
       } else {
         digitalWrite(colPins[j], HIGH);
@@ -56,5 +64,24 @@ void displayLetterH() {
       }
     }
     delay(2); 
+  }
+}
+void displayLetterC(){
+  for(int i=0; i<hc ;i++){
+    for (int j=0; j<numCols ; j++){
+      if(letterC_vertical[i].charAt(j) == '0'){
+        digitalWrite(colPins[j], LOW);
+      }
+      else{
+        digitalWrite(colPins[j],HIGH);
+      }
+      if(letterC_horizontal[i].charAt(j) == '0'){
+        digitalWrite(rowPins[j], LOW);
+      }
+      else{
+        digitalWrite(rowPins[j], HIGH);
+      }
+    }
+    delay(2);
   }
 }
